c1b0702539c2c1d5ffab844fbd1de96590afc1e9
Ark P <Arkasha960@gmail.com>
2025-04-18 17:00:56 +0300

    +script, изменено название контроллера

---
+++ data/Scripts/WatchTarget.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "475fce5a9a1456239d694c3cdc5c1b4e8d6485c2")]
+public class WatchTarget : Component
+{
+	void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	void Update()
+	{
+		// write here code to be called before updating each render frame
+		
+	}
+}
